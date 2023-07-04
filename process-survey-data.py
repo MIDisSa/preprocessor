9c278ccc560f606e6943e485e38bc9bbac2f5063
hanuta27 <hannah.rohe@hispeed.ch>
2023-07-04 09:49:04 +0200

    create dataframe for results and export to csv in the end

--- process-survey-data.py
+++ process-survey-data.py
@@ -4,6 +4,12 @@ import pandas as pd
 # read csv file
 df = pd.read_csv("data.csv", header=0, decimal=",")
 
+# create new dataframe 
+## add derived data into this dataframe
+## this dataframe will be exported as csv file to be passed to the automator
+## columns are named after parameter names in netlogo model
+df_results = pd.DataFrame(columns=['nr_default_friends_inter_village', 'avg_intra_village_interaction_frequency', 'avg_inter_village_interaction_frequency', 'avg_chief_farmer_meeting_frequency', 'farmgroup_meeting_attendance_percentage', 'avg_intra_mention_percentage', 'avg_inter_mention_percentage', 'avg_meeting_mention_percentage', 'percentage_negative_WoM', 'base_adoption_probability'])
+
 # process data
 ## avg-nr-inter-village-friends
 
@@ -20,4 +26,4 @@ df = pd.read_csv("data.csv", header=0, decimal=",")
 ## base-adoption-probability
 
 # export data as csv file
-df.to_csv("data-processed.csv")
+df_results.to_csv("data-processed.csv")
